c91a8148020c2103db126ff3ab00ae053e7e6cb1
Artur Bożek <arturhubert.bozek@gmail.com>
2023-07-18 22:00:40 +0200

    wydzielanie komponentow

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,8 @@
 import styles from './Product.module.scss';
-import clsx from 'clsx';
-import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import ProductImage from '../ProductImage/ProductImage';
+import OptionsForm from '../OptionsForm/OptionsForm';
 
 const Product = (props) => {
 
@@ -10,9 +10,9 @@ const Product = (props) => {
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
   const [currentPrice, setCurrentPrice] = useState(props.sizes[0].additionalPrice);
 
-  console.log('currentColor: ', currentColor);
-  console.log('currentSize: ', currentSize);
-  console.log('currentPrice: ', currentPrice);
+  // console.log('currentColor: ', currentColor);
+  // console.log('currentSize: ', currentSize);
+  // console.log('currentPrice: ', currentPrice);
 
   const prepareColorClassName = (color) => {
     return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
@@ -22,40 +22,34 @@ const Product = (props) => {
     return props.basePrice + currentPrice;
   };
 
+  const Summary = (e) => {
+    e.preventDefault();
+    console.log('=== Summary ===',
+    'Name:', props.title,
+    'Color:', currentColor,
+    'Size:', currentSize,
+    'price:', getPrice()
+    );
+  };
+
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-      <img className={styles.image} alt={`${props.title} - ${currentColor} - ${currentSize}`} size={currentSize} src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
-      </div>
-      <div>
+      <ProductImage name={props.name} color={currentColor} size={currentSize} />
+    <div>
         <header>
-        <h2 className={styles.name}>{props.title}</h2>
+          <h2 className={styles.name}>{props.title}</h2>
           <span className={styles.price}>{getPrice()}$</span>
         </header>
         <form>
-          <div className={styles.sizes}>
-            <h3 className={styles.optionLabel}>Sizes</h3>
-            <ul className={styles.choices}>
-            {props.sizes.map((size) => (
-                <li key={size}>
-                  <button type="button" className={clsx(styles.size, size.name === currentSize && styles.active)} onClick = {(e) => {e.preventDefault(); setCurrentSize(size.name); setCurrentPrice(size.additionalPrice)}}>{size.name}</button>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.colors}>
-            <h3 className={styles.optionLabel}>Colors</h3>
-            <ul className={styles.choices}>
-            {props.colors.map((item) => (
-                <li key={item}>
-                    <button type="button" className={clsx(prepareColorClassName(item), item === item && styles.active)} onClick = {(e) => {e.preventDefault(); setCurrentColor(item);}}></button>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <Button className={styles.button} onClick={() => {console.log("Name")}}>
-            <span className="fa fa-shopping-cart" />
-          </Button>
+          <OptionsForm
+            sizes={props.sizes}
+            currentSize={currentSize} 
+            currentColor={currentColor}
+            setCurrentSize={setCurrentSize} 
+            setCurrentPrice={setCurrentPrice} 
+            colors={props.colors}
+            setCurrentColor={setCurrentColor}
+            Summary={Summary} />
         </form>
       </div>
     </article>
